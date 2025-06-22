7e79844ae88a0990dccf01ef5bd35eee59dbdd18
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-22 21:17:43 +0700

    init bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds
+    ]
+})
+
+client.on("ready", () => console.log(`Logged in as ${client.user?.tag}`))
+
+client.login("")
