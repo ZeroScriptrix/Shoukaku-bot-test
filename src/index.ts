04e62384466c6d065e961ac838cba7d9b140f218
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-23 20:37:02 +0700

    format

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import {
     GatewayIntentBits,
     Message,
     OmitPartialGroupDMChannel,
-} from "discord.js";
-import { Shoukaku, Player, Connectors, Track, LoadType } from "shoukaku";
+} from 'discord.js';
+import { Shoukaku, Player, Connectors, Track, LoadType } from 'shoukaku';
 
 const client = new Client({
     intents: [
@@ -19,37 +19,37 @@ const client = new Client({
 // see below for more info
 const Nodes = [
     {
-        name: "Localhost",
-        url: "lavahatry4.techbyte.host:3000",
-        auth: "NAIGLAVA-dash.techbyte.host",
+        name: 'Localhost',
+        url: 'lavahatry4.techbyte.host:3000',
+        auth: 'NAIGLAVA-dash.techbyte.host',
     },
 ];
 
 const shoukaku = new Shoukaku(new Connectors.DiscordJS(client), Nodes);
 
-shoukaku.on("ready", (node) => console.log(`Node is Ready ${node}`));
+shoukaku.on('ready', (node) => console.log(`Node is Ready ${node}`));
 
 const playerMap = new Map<string, Player>();
 
 client.on(
-    "messageCreate",
+    'messageCreate',
     async (message: OmitPartialGroupDMChannel<Message>) => {
         if (message.author.bot) return;
 
-        if (message.content.startsWith("!play")) {
-            const input = message.content.split(" ")[1];
+        if (message.content.startsWith('!play')) {
+            const input = message.content.split(' ')[1];
 
-            if (!input) return message.channel.send("No track to load");
+            if (!input) return message.channel.send('No track to load');
             if (!message.member?.voice.channelId)
-                return message.channel.send("Not in a voice channel");
-            if (!message.guild) return message.channel.send("Guild not found");
+                return message.channel.send('Not in a voice channel');
+            if (!message.guild) return message.channel.send('Guild not found');
 
             const node = shoukaku.getIdealNode();
-            if (!node) return message.channel.send("No nodes available");
+            if (!node) return message.channel.send('No nodes available');
 
             const resolved = await node.rest.resolve(input);
             if (!resolved || resolved.loadType === LoadType.EMPTY)
-                return message.channel.send("Track not found");
+                return message.channel.send('Track not found');
 
             const track = resolved.data as Track;
             let player = playerMap.get(message.guild.id);
@@ -65,30 +65,30 @@ client.on(
                 playerMap.set(message.guild.id, player);
             }
 
-            if (!player) return message.reply("ไม่สามารถสร้าง player ได้");
+            if (!player) return message.reply('ไม่สามารถสร้าง player ได้');
             return player.playTrack({ track: { encoded: track.encoded } });
         }
 
-        if (message.content.startsWith("!stop")) {
-            if (!message.guild) return message.channel.send("Guild not found");
+        if (message.content.startsWith('!stop')) {
+            if (!message.guild) return message.channel.send('Guild not found');
 
             const player = playerMap.get(message.guild.id);
-            if (!player) return message.channel.send("No active player found");
+            if (!player) return message.channel.send('No active player found');
 
             shoukaku.leaveVoiceChannel(message.guild.id);
             playerMap.delete(message.guild.id);
-            return message.channel.send("Ok!");
+            return message.channel.send('Ok!');
         }
 
-        if (message.content.startsWith("!nightcore")) {
-            if (!message.guild) return message.channel.send("Guild not found");
+        if (message.content.startsWith('!nightcore')) {
+            if (!message.guild) return message.channel.send('Guild not found');
             const player = playerMap.get(message.guild.id);
-            if (!player) return message.channel.send("No active player found");
+            if (!player) return message.channel.send('No active player found');
 
             await player.setTimescale({ speed: 1.2, pitch: 1.2, rate: 1.2 });
-            return message.channel.send("Nightcore activated!");
+            return message.channel.send('Nightcore activated!');
         }
-    },
+    }
 );
 
-client.login("");
+client.login('');
